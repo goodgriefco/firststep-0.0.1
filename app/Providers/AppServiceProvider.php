da8277d7a18b94f7dc0157642905d63fcbddf321
Shae Smith <sbsmith86@gmail.com>
2019-05-19 16:55:11 -0400

    add support for longer strings in db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,8 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
+
     /**
      * Register any application services.
      *

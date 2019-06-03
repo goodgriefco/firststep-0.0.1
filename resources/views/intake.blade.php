6493025811cbdf80bc0f2a0f39390a799d0ab3fc
Shae Smith <sbsmith86@gmail.com>
2019-06-02 23:02:22 -0400

    add user id to intake page

--- resources/views/intake.blade.php
+++ resources/views/intake.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 {{-- @TODO - form id should NOT be hardcoded in url --}}
-    <div class="typeform-widget" data-url={{'https://goodgriefco.typeform.com/to/YG2CwN?user='.$userId}} style="width: 100%; height: 500px;"></div>
+    <div class="typeform-widget" data-url={{'https://goodgriefco.typeform.com/to/YG2CwN?user='.$userId}} data-user-id={{$userId}} style="width: 100%; height: 500px;"></div>
     <script>
         (function() {
             var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/";
